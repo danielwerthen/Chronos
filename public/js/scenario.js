7632e5cba232ed5ea43ffe30a8a4bf7ad70535e3
Daniel Werthén <danielwerthen@gmail.com>
2012-09-26 19:35:12 +0200

    Message

--- public/js/scenario.js
+++ public/js/scenario.js
@@ -17,7 +17,7 @@ define(['connector'
 	var helan
 		, halvan
 	//en fjärde del, åttondels paus, åttondel + fjärdedel, fjärdedel pause
-	render.onLoop(function (nr, len) {
+	function baseline(nr, len) {
 		helan.animate({ radius: 400, opacity: 0 }, len / 1.5, { type: 'inOut', fn: 'exp' }, function() {
 			this.animate({ radius: 0, opacity: 0.5 });
 		});
@@ -26,13 +26,17 @@ define(['connector'
 				this.animate({ radius: 0, opacity: 0.5 });
 			});
 		}, (len * (3 / 8)));
+	}
+
+	render.onLoop(function (nr, len) {
+		baseline(nr, len);
 	});
 	
 	render.onInit(drawObjects);
 	function drawObjects() {
-		helan = drawCircle(x(0.75), y(0.5), '#FFF', 0, 'helan', 'baseline');
+		helan = drawCircle(x(0.75), y(0.5), '#003780', 0, 'helan', 'baseline');
 		helan.animate({ opacity: 0.5 });
-		halvan = drawCircle(x(0.25), y(0.5), '#FFF', 0, 'halvan', 'baseline');
+		halvan = drawCircle(x(0.25), y(0.5), '#003780', 0, 'halvan', 'baseline');
 		halvan.animate({ opacity: 0.5 });
 	}
 });
