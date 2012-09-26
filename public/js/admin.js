d263ee138217341ff54e776424b0514243e2c5bf
Daniel Werthén <danielwerthen@gmail.com>
2012-09-26 15:28:08 +0200

    Moose

--- public/js/admin.js
+++ public/js/admin.js
@@ -1,3 +1,10 @@
+requirejs.config({
+	paths: {
+		'jquery': 'http://ajax.googleapis.com/ajax/libs/jquery/1.7.2/jquery.min'
+	, 'socket.io': '/socket.io/socket.io'
+	}
+});
+
 require([
 		'connector'
 		, 'jquery'
